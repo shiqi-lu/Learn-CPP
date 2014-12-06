55b519e515632dad478ff751818a1a12ce5d9637
TraumLou <traumlou@gmail.com>
2014-12-06 08:49:40 +0800

    stl vector

---
+++ stl/container/vector.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void init_vector()
+{
+    cout << "=========== init_vector: ===========" << endl;
+    vector<string> myvt;
+    // optional
+    myvt.reserve(4);
+    myvt.push_back("1.Beijing City.");
+    myvt.push_back("2.Tianjin City.");
+    myvt.push_back("3.Shanghai City.");
+    myvt.push_back("4.Chongqing City.");
+    vector<string>::iterator it;
+    for (it = myvt.begin(); it != myvt.end(); ++it) {
+        cout << *it << endl;
+    }
+    cout << "size is " << myvt.size() << endl;
+    cout << "capacity is " << myvt.capacity() << endl;
+    cout << "maxsize is " << myvt.max_size() << endl;
+    myvt.resize(10);    //重置大小
+    cout << "resize: 10" << endl;
+    cout << "size is " << myvt.size() << endl;
+    cout << "capacity is " << myvt.capacity() << endl;
+    for (it = myvt.begin(); it != myvt.end(); ++it) {
+        if (*it == "") {
+            cout << "*********" << endl;
+        }
+        cout << *it << endl;
+    }
+    
+}
+
+int main()
+{
+    init_vector();   
+
+    return 0;
+}
