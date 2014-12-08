55cea4ba2da3a440e0bf153114e99a974c55c471
TraumLou <traumlou@gmail.com>
2014-12-08 20:03:27 +0800

    stl vector

--- stl/container/vector.cpp
+++ stl/container/vector.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 using namespace std;
+
+void print_myvt(string & tmp)
+{
+    cout << "the city is : " << tmp << endl;
+}
 
 void init_vector()
 {
@@ -17,49 +23,76 @@ void init_vector()
     for (it = myvt.begin(); it != myvt.end(); ++it) {
         cout << *it << endl;
     }
+    // not recommend
+    for (int i = 0; i < 4; ++i) {
+        cout << myvt.at(i) << endl;
+    }
+    for_each(myvt.begin(), myvt.end(), print_myvt);
     cout << "size is " << myvt.size() << endl;
     cout << "capacity is " << myvt.capacity() << endl;
     cout << "maxsize is " << myvt.max_size() << endl;
-    myvt.resize(10);    //重置大小
-    cout << "resize: 10" << endl;
+    myvt.resize(7);    //重置大小
+    cout << "resize: 7" << endl;
     cout << "size is " << myvt.size() << endl;
     cout << "capacity is " << myvt.capacity() << endl;
     for (it = myvt.begin(); it != myvt.end(); ++it) {
         if (*it == "") {
             cout << "*********" << endl;
+            continue;
         }
         cout << *it << endl;
     }
+    cout << endl <<  "empty: " << endl;
+    string tmp;
+    while(!myvt.empty()) {
+        tmp = myvt.back();
+        cout << tmp << endl;
+        myvt.pop_back();
+    }
+    if (!myvt.empty()) {
+        myvt.clear();
+    }
 /*
-output
+output:
 =========== init_vector: ===========
 1.Beijing City.
 2.Tianjin City.
 3.Shanghai City.
 4.Chongqing City.
+1.Beijing City.
+2.Tianjin City.
+3.Shanghai City.
+4.Chongqing City.
+the city is : 1.Beijing City.
+the city is : 2.Tianjin City.
+the city is : 3.Shanghai City.
+the city is : 4.Chongqing City.
 size is 4
 capacity is 4
 maxsize is 2305843009213693951
-resize: 10
-size is 10
-capacity is 10
+resize: 7
+size is 7
+capacity is 8
 1.Beijing City.
 2.Tianjin City.
 3.Shanghai City.
 4.Chongqing City.
 *********
-
+*********
 *********
 
-*********
+empty: 
 
-*********
 
-*********
 
-*********
+4.Chongqing City.
+3.Shanghai City.
+2.Tianjin City.
+1.Beijing City.
 */
 }
+
+
 
 int main()
 {
