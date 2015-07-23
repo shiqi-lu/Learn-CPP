ddec84224c62d4424b8543d430e3cb1f7225fa78
TraumLou <traumlou@gmail.com>
2015-07-24 07:39:25 +0800

    streamio - stream.c

---
+++ linux/streamio/stream.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+int main()
+{
+    FILE * fp;
+    int fd;
+    if ((fp = fopen("test.txt", "w+")) == NULL) {
+        perror("fail to open");
+        exit(1);
+    }
+    fprintf(fp, "hello world\n");
+    fclose(fp);
+
+    if ((fd = open("test.txt", O_RDWR)) == -1) {
+        perror("fail to open");
+        exit(1);
+    }
+
+    if ((fp= fdopen(fd, "a")) == NULL) {
+        perror("fail to open stream");
+        exit(1);
+    }
+
+    fprintf(fp, "hello world again\n");
+    fclose(fp);
+
+    return 0;
+}
