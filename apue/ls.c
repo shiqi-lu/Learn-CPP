59950dac617f95591bbf05a2aba9994ec192290e
TraumLou <traumlou@gmail.com>
2015-12-01 07:27:35 +0800

    apue - ls.c

---
+++ apue/ls.c
@@ -0,0 +1,24 @@
+#include "apue.h"
+#include <dirent.h>
+
+int
+main(int argc, char * argv[])
+{
+    DIR * dp;
+    struct dirent * dirp;
+
+    if (argc != 2) {
+        err_quit("Usage: is directory_name");
+    }
+
+    if ((dp = opendir(argv[1])) == NULL) {
+        err_sys("Can't open %s", argv[1]);
+    }
+    while ((dirp = readdir(dp)) != NULL) {
+        printf("%s\n", dirp->d_name);
+    }
+
+    closedir(dp);
+
+    exit(0);
+}
